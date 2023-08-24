0f70db737c00d72be20fd9d3b96fe3f76bb32f74
Alexander Gorbatenko <alexander@mailup.net>
2023-08-24 11:06:33 +0300

    Правки

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,18 +7,18 @@ const NotAuthorizedError = require('../errors/NotAuthorizedError');
 function auth(req, res, next) {
   const { authorization } = req.headers;
   const err = new NotAuthorizedError('Необходима авторизация');
+  let payload;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(err);
-  }
+  } else {
+    const token = authorization.replace('Bearer ', '');
 
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-
-  try {
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (e) {
-    next(err);
+    try {
+      payload = jwt.verify(token, JWT_SECRET);
+    } catch (e) {
+      next(err);
+    }
   }
 
   req.user = payload;
